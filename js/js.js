243a30eafead72440924ef2386464c1c24001548
Ivan <vanya.grigorevskiy@mail.ru>
2020-11-23 17:02:01 +0300

    added mobile menu

--- js/js.js
+++ js/js.js
@@ -1,6 +1,24 @@
 function open_mobile_menu(){
 	var mobile_menu = document.getElementsByClassName('mobile-menu');
 	for (var i = 0; i < mobile_menu.length; i++) {
-		mobile_menu[i].classList.add("open-mobile-menu");
+		mobile_menu[i].classList.add("display-none");
 	}
+	var mobile_menu_opened = document.getElementsByClassName('mobile-menu-opened');
+	for (var i = 0; i < mobile_menu_opened.length; i++) {
+		mobile_menu_opened[i].classList.remove("display-none");
+	}
+	document.body.style.overflow = "hidden";
+}
+
+function close_mobile_menu(){
+	var mobile_menu = document.getElementsByClassName('mobile-menu');
+	for (var i = 0; i < mobile_menu.length; i++) {
+		mobile_menu[i].classList.remove("display-none");
+	}
+	var mobile_menu_opened = document.getElementsByClassName('mobile-menu-opened');
+	for (var i = 0; i < mobile_menu_opened.length; i++) {
+		mobile_menu_opened[i].classList.add("display-none");
+	}
+	document.body.style.overflowX = "hidden";
+	document.body.style.overflowY = "auto";
 }
