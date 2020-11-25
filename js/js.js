bb2ce849e586a0bb0bc678fba04e48ff712cc949
Ivan Grigor <vanya.grigorevskiy@gmail.com>
2020-11-25 10:17:28 +0300

    added mobile-menu animation

--- js/js.js
+++ js/js.js
@@ -1,23 +1,23 @@
 function open_mobile_menu(){
-	var mobile_menu = document.getElementsByClassName('mobile-menu');
+	var mobile_menu = document.getElementsByClassName('mobile-menu-icon');
 	for (var i = 0; i < mobile_menu.length; i++) {
 		mobile_menu[i].classList.add("display-none");
 	}
 	var mobile_menu_opened = document.getElementsByClassName('mobile-menu-opened');
 	for (var i = 0; i < mobile_menu_opened.length; i++) {
-		mobile_menu_opened[i].classList.remove("display-none");
+		mobile_menu_opened[i].classList.add("top-0");
 	}
 	document.body.style.overflow = "hidden";
 }
 
 function close_mobile_menu(){
-	var mobile_menu = document.getElementsByClassName('mobile-menu');
+	var mobile_menu = document.getElementsByClassName('mobile-menu-icon');
 	for (var i = 0; i < mobile_menu.length; i++) {
 		mobile_menu[i].classList.remove("display-none");
 	}
 	var mobile_menu_opened = document.getElementsByClassName('mobile-menu-opened');
 	for (var i = 0; i < mobile_menu_opened.length; i++) {
-		mobile_menu_opened[i].classList.add("display-none");
+		mobile_menu_opened[i].classList.remove("top-0");
 	}
 	document.body.style.overflowX = "hidden";
 	document.body.style.overflowY = "auto";
